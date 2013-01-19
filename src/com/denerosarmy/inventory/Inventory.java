f1ca6d7910f5e0ba57e7255f75717d05d9f6cae2
vaishaal <vaishaal@vaishaals-MacBook-Air.local>
2013-01-19 17:37:54 -0500

    Bluetooth Works

--- src/com/denerosarmy/inventory/Inventory.java
+++ src/com/denerosarmy/inventory/Inventory.java
@@ -2,6 +2,14 @@ package com.denerosarmy.inventory;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Handler;
+import android.widget.Toast;
+import android.os.Message;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.SearchView;
@@ -16,11 +24,29 @@ import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+
 
 public class Inventory extends Activity{
 
     ListView container;
     NotificationManager notificationManager;
+    private static final String TAG = "Inventory"; 
+    private String backpackId= null;
+    private static final boolean D = true;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private BluetoothChatService mChatService = null;
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    private static final int REQUEST_ENABLE_BT = 3;
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast"; 
+ 
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,6 +96,16 @@ public class Inventory extends Activity{
 
         this.registerReceiver(this.WifiStateChangedReceiver,
                               new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
+        
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+ 
 
     }
 
@@ -118,5 +154,105 @@ public class Inventory extends Activity{
         notificationManager.notify(Integer.parseInt(item.getId()), noti); 
 
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(D) Log.e(TAG, "++ ON START ++");
+
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+        // Otherwise, setup the chat session
+        } else {
+            if (mChatService == null) { 
+                mChatService = new BluetoothChatService(this,mHandler); 
+            }
+        }
+    }
+    @Override
+    public synchronized void onPause() {
+        super.onPause();
+        if(D) Log.e(TAG, "- ON PAUSE -");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(D) Log.e(TAG, "-- ON STOP --");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Stop the Bluetooth chat services
+        if (mChatService != null) mChatService.stop();
+        if(D) Log.e(TAG, "--- ON DESTROY ---");
+    }
+    @Override
+    public synchronized void onResume() {
+        super.onResume();
+        if(D) Log.e(TAG, "+ ON RESUME +");
+
+        // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mChatService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+              // Start the Bluetooth chat services
+              mChatService.start();
+            }
+        }
+    }
+    
+    public void process(String value){}
+
+         
+    private final Handler mHandler = new Handler() {
+        @Override
+        public synchronized void handleMessage(Message msg) {
+            switch (msg.what) {
+            case MESSAGE_STATE_CHANGE:
+                if(D) Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+                switch (msg.arg1) {
+                case BluetoothChatService.STATE_CONNECTED:
+                    break;
+                case BluetoothChatService.STATE_CONNECTING:
+                    break;
+                case BluetoothChatService.STATE_LISTEN:
+                case BluetoothChatService.STATE_NONE:
+                    break;
+                }
+                break;
+            case MESSAGE_WRITE:
+                byte[] writeBuf = (byte[]) msg.obj;
+                // construct a string from the buffer
+                String writeMessage = new String(writeBuf);
+                break;
+            case MESSAGE_READ:
+                byte[] readBuf = (byte[]) msg.obj;
+               
+                // construct a string from the valid bytes in the buffer
+                String readMessage = new String(readBuf, 0, msg.arg1);
+                System.out.println(readMessage); 
+                process(readMessage);
+                break;
+            case MESSAGE_DEVICE_NAME:
+                // save the connected device's name
+                backpackId = msg.getData().getString(DEVICE_NAME);
+                Toast.makeText(getApplicationContext(), "Connected to "
+                               + backpackId, Toast.LENGTH_SHORT).show();
+                break;
+            case MESSAGE_TOAST:
+                Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+                               Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+    };
+
+
 
 }

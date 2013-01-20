5c4b759779a31d3e0bef88378b62d973032f8208
Jian Leong <jianwei1801@gmail.com>
2013-01-20 04:28:49 -0800

    added null handling

--- src/com/denerosarmy/inventory/Matcher.java
+++ src/com/denerosarmy/inventory/Matcher.java
@@ -3,6 +3,9 @@ package com.denerosarmy.inventory;
 public class Matcher{
 
     protected static boolean matches(String s1, String s2){
+        if ((s1 == null)||(s2 == null)){
+            return false;
+        }
         return (s1.toLowerCase()).equals(s2.toLowerCase());
     }
 

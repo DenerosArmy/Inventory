d5afdab7c9af7d14060c474ede7ac55e85c380c0
Richie Zeng <richzeng@gmail.com>
2013-01-19 17:31:11 -0800

    set up item adding stuff. still have some issues with Wifi

--- src/com/denerosarmy/inventory/Inventory.java
+++ src/com/denerosarmy/inventory/Inventory.java
@@ -1,58 +1,73 @@
 package com.denerosarmy.inventory;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.SearchView;
-import android.app.SearchManager;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.content.Context;
-import android.widget.ListView;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.SearchManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
-import android.content.BroadcastReceiver;
-import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SearchView;
 
 public class Inventory extends Activity{
 
     ListView container;
     NotificationManager notificationManager;
+    static boolean initialized;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	System.out.println("Initialized is " + initialized);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-     
-        container = (ListView) findViewById(R.id.compartments);
-        Compartment c1 = new Compartment("1", "Main");
-        Compartment c2 = new Compartment("2", "Misc");
-        Compartment c3 = new Compartment("3", "Miscer");
-        Item i0 = new Item("0", "Pencil", R.drawable.sample_0);
-        Item i1 = new Item("1", "Banana", R.drawable.sample_1);
-        Item i2 = new Item("2", "Popsicle", R.drawable.sample_2);
-        Item i3 = new Item("3", "Pencil", R.drawable.sample_3);
-        Item i4 = new Item("4", "Flour", R.drawable.sample_4);
-        Item i5 = new Item("5", "Hair", R.drawable.sample_5);
-        Item i6 = new Item("6", "Sand", R.drawable.sample_6);
-        Item i7 = new Item("7", "Aluminum Foil", R.drawable.sample_7);
-        Item i8 = new Item("8", "Rubbish", R.drawable.sample_0);
-        Item i9 = new Item("9", "Pencil", R.drawable.sample_3);
+	    container = (ListView) findViewById(R.id.compartments);
+	    
+	    if (!initialized) {
 
-        i0.putInto("1");
-        i1.putInto("1");
-        i2.putInto("1");
-        i2.remove();
-        i3.putInto("1");
-        i4.putInto("1");
-        i5.putInto("1");
-        i6.putInto("2");
-        i7.putInto("2");
-        i8.putInto("2");
-        i9.putInto("3");
+	        Compartment c1 = new Compartment("1", "Drawer");
+	        Compartment c2 = new Compartment("2", "Secondary");
+	        Compartment c3 = new Compartment("3", "Main");
+	        Item i0 = new Item("0", "Pencil", R.drawable.sample_0);
+	        Item i1 = new Item("1", "Banana", R.drawable.sample_1);
+	        Item i2 = new Item("2", "Popsicle", R.drawable.sample_2);
+	        Item i3 = new Item("3", "Pencil", R.drawable.sample_3);
+	        Item i4 = new Item("4", "Flour", R.drawable.sample_4);
+	        Item i5 = new Item("5", "Hair", R.drawable.sample_5);
+	        Item i6 = new Item("6", "Sand", R.drawable.sample_6);
+	        Item i7 = new Item("7", "Aluminum Foil", R.drawable.sample_7);
+	        Item i8 = new Item("8", "Rubbish", R.drawable.sample_0);
+	        Item i9 = new Item("9", "Pencil", R.drawable.sample_3);
+
+	        i0.putInto("1");
+	        i1.putInto("3");
+	        i2.putInto("1");
+	        i2.remove();
+	        i3.putInto("3");
+	        i4.putInto("3");
+	        i5.putInto("3");
+	        i6.putInto("2");
+	        i7.putInto("2");
+	        i8.putInto("2");
+	        i9.putInto("3");
+        }
+        if (initialized) {
+            System.out.println("AWEFHDSIUJUROEWRHUGFOJRATHUGJFAOIRHEGUJAFSIORGHEJ");
+		    try {
+		    	Intent intent = getIntent();
+		    	System.out.println(intent.toString());
+		        String id = intent.getStringExtra(ItemCreate.ITEM_ID);
+		        String name = intent.getStringExtra(ItemCreate.ITEM_NAME);
+		        String comp = intent.getStringExtra(ItemCreate.ITEM_COMPARTMENT);
+		        new Item(id, name, R.drawable.olivia_wilde).putInto(comp);
+		    } catch (NullPointerException e) {
+		    }
+        }
+        initialized = true;
 
         container.setAdapter(new CompartmentAdapter(this, Container.inst().getComps()));
 
@@ -61,12 +76,8 @@ public class Inventory extends Activity{
         SearchView searchView = (SearchView) findViewById(R.id.searchView);
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setIconifiedByDefault(false); // Do not iconify the widget; expand it by default
-        searchView.setSubmitButtonEnabled(true);
 
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-        System.out.println(i1.inContainer());
-        System.out.println(i2.inContainer());
 
         this.registerReceiver(this.WifiStateChangedReceiver,
                               new IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION));
@@ -117,5 +128,10 @@ public class Inventory extends Activity{
 
         notificationManager.notify(Integer.parseInt(item.getId()), noti); 
 
+    }
+    
+    public void createItem(View view) {
+    	Intent intent = new Intent(this, ItemCreate.class);
+        startActivity(intent);
     }
 }
---
+++ src/com/denerosarmy/inventory/ItemCreate.java
@@ -0,0 +1,52 @@
+package com.denerosarmy.inventory;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+/**
+ * Activity which displays a form to the user
+ */
+public class ItemCreate extends Activity {
+	
+    public final static String ITEM_NAME = "com.denerosarmy.inventory.ITEM_NAME";
+    public final static String ITEM_ID = "com.denerosarmy.inventory.ITEM_ID";
+    public final static String ITEM_COMPARTMENT = "com.denerosarmy.inventory.ITEM_COMPARTMENT";
+
+    @SuppressLint("NewApi")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_item_create);
+	}
+    
+    public void goBack(View view) {
+    	Intent intent = new Intent(this, Inventory.class);
+        startActivity(intent);
+    }
+
+	public void addItem(View view) {
+    	Intent intent = new Intent(this, Inventory.class);
+    	EditText nameView = (EditText) findViewById(R.id.itemName);
+    	String name = nameView.getText().toString();
+    	intent.putExtra(ITEM_NAME, name);
+
+    	EditText idView = (EditText) findViewById(R.id.rfid);
+    	String id = idView.getText().toString();
+    	intent.putExtra(ITEM_ID, id);
+    	
+    	EditText compView = (EditText) findViewById(R.id.compartment);
+    	String comp = compView.getText().toString();
+    	intent.putExtra(ITEM_COMPARTMENT, comp);
+
+        startActivity(intent);
+	}
+
+}

ce89f763fa8ce4d367e3ef80e3479f6e943684eb
Richie Zeng <richzeng@gmail.com>
2013-02-17 00:26:08 -0800

    removed build files

--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/vaishaal/Inventory/bin/AndroidManifest.xml : \
-/Users/vaishaal/Inventory/AndroidManifest.xml \
--- bin/Inventory-debug-unaligned.apk.d
+++
@@ -1,7 +0,0 @@
-/Users/vaishaal/Inventory/bin/Inventory-debug-unaligned.apk : \
-/Users/vaishaal/Inventory/bin/Inventory.ap_ \
-/Users/vaishaal/Inventory/bin/classes.dex \
-/Users/vaishaal/Inventory/src/com/denerosarmy/inventory/:w \
-/Users/vaishaal/Inventory/src/com/denerosarmy/inventory/RequestTask \
-/Users/vaishaal/Inventory/src/com/denerosarmy/inventory/socketError.dump \
-/Users/vaishaal/Inventory/libs/android-support-v4.jar \
